9a478261e9d094edcf06d2e57ff585a9bec81827
GamerLucas2 <lucasonckent@hotmail.com>
2026-03-25 12:24:51 -0300

    Shortcuts
    - Fixed Shortcuts, so they appear and disapear
    - Made so shortcuts only appaear if the player activates the siren
    - Siren activate button
    - Siren battery depletion

--- GGJ-Next-2026/Assets/Script/GameManager.cs
+++ GGJ-Next-2026/Assets/Script/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
@@ -5,11 +6,15 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI CounterText;
     [SerializeField] public float batteryCounter = 100f;
-    public bool hasBattery = true;
+    private bool hasBattery = true;
+    public bool sirenActive = false;
+
     void Start()
     {
         batteryCounter = 100f;
+        sirenActive = false;
     }
+
     void Update()
     {
         CounterText.text = batteryCounter.ToString("Bateria: 0");
@@ -18,11 +23,26 @@ public class GameManager : MonoBehaviour
         {
             batteryCounter = 0f;
             hasBattery = false;
+            sirenActive = false;
         }
+        else if (batteryCounter > 0f)
+        {
+            hasBattery = true;
+
+        }
+
         if (batteryCounter >= 100f)
             batteryCounter = 100f;
+
+        if (Input.GetKeyDown(KeyCode.R) && hasBattery && batteryCounter == 100f)
+        {
+            sirenActive = true;
+        }
+
+        if (sirenActive)
+            batteryCounter -= Time.deltaTime * 15f;
     }
-    
+
     public void DecreaseBattery(float amount)
     {
         batteryCounter -= amount;
