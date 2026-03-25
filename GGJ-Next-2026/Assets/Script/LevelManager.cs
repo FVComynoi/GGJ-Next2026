f7d6454bef4a50e1e31a24f70df88c59b4a96014
FVComynoi <felipecomynoi@gmail.com>
2026-03-24 21:46:27 -0300

    UI and Level End
    -Play button
    -Play again button
    -Level End (triggers play again)

---
+++ GGJ-Next-2026/Assets/Script/LevelManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameEndScreen;
+    [SerializeField] GameObject playButton;
+    private void Awake()
+    {
+        Time.timeScale = 0f;
+        gameEndScreen.SetActive(false);
+        playButton.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            GameEnd();
+        }
+    }
+    public void GameEnd()
+    {
+        gameEndScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void OnPlay()
+    {
+        playButton.SetActive(false);
+        Time.timeScale = 1f;
+    }        
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+}
